7033bfca57f4361f6b24c7b464361e04d07aa3d7
Bipro23 <88981554+Bipro23@users.noreply.github.com>
2023-09-25 21:09:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+window.addEventListener("resize", getscreensize);
+
+function getscreensize(){
+	let element = document.getElementById("sizeInfo");
+	let x = document.documentElement.clientWidth;
+	let y = document.documentElement.clientHeight;
+	element.innerHTML= `<h1>Width: ${x} and Height: ${y}</h1>`;
+}
